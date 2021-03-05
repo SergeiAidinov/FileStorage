3fb8bb07d9d99f27c77b3e092523ae926b3dc6ae
Sergei <sergei@debian.ru>
2021-03-05 14:29:27 +0300

    First commit

---
+++ src/main/java/ru/yandex/incoming34/filestorage/Client/Client.java
@@ -0,0 +1,147 @@
+package ru.yandex.incoming34.filestorage.Client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Client {
+	private final Socket socket;
+	private final DataInputStream in;
+	private final DataOutputStream out;
+        JTextArea ta;
+
+	public Client() throws IOException {
+		socket = new Socket("localhost", 1235);
+		in = new DataInputStream(socket.getInputStream());
+		out = new DataOutputStream(socket.getOutputStream());
+		runClient();
+	}
+
+	private void runClient() {
+		JFrame frame = new JFrame("Cloud Storage");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 300);
+
+		ta = new JTextArea();
+		// TODO: 02.03.2021
+		// list targetFile - JList
+		JButton uploadButton = new JButton("Upload");
+                JButton deleteButton = new JButton("Delete");
+                JButton downloadButton = new JButton("Download");
+                JButton listOfFiles = new JButton("List");
+		frame.getContentPane().add(BorderLayout.CENTER, ta);
+                Container container = new Container();
+                container.setLayout(new FlowLayout());
+		container.add(uploadButton);
+                container.add(deleteButton);
+                container.add(downloadButton);
+                container.add(listOfFiles);
+                frame.getContentPane().add(BorderLayout.SOUTH, container);
+		frame.setVisible(true);
+		uploadButton.addActionListener(upl -> {
+			System.out.println(sendFile(ta.getText()));
+		});
+                deleteButton.addActionListener(dlt -> {
+                    System.out.println(deleteFile(ta.getText()));
+                });
+                downloadButton.addActionListener(dnl -> {
+                    System.out.println(downloadFile(ta.getText()));
+                });
+                listOfFiles.addActionListener(lst -> {
+                    System.out.println(showListOfFiles());
+                });
+                
+	}
+
+	private String sendFile(String filename) {
+            FileInputStream fis = null;
+            if (filename.length() == 0) {
+                return("File name needed.");
+                
+            }
+		try {
+			File file = new File("/media/sergei/Linux/ClientFiles/" + File.separator + filename);
+			if (file.exists()) {
+				out.writeUTF("upload");
+				out.writeUTF(filename);
+				long length = file.length();
+				out.writeLong(length);
+				fis = new FileInputStream(file);
+				int read = 0;
+				byte[] buffer = new byte[256];
+				while ((read = fis.read(buffer)) != -1) {
+					out.write(buffer, 0, read);
+				}
+				out.flush();
+				String status = in.readUTF();
+                                fis.close();
+				return status;
+			} else {
+				return "File " + file + " does not exists";
+                                
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		return "Something error";
+	} 
+
+	public static void main(String[] args) throws IOException {
+		new Client();
+	}
+
+    private String deleteFile(String filename) {
+        String status = null;
+            try {
+                out.writeUTF("remove");
+                out.writeUTF(filename);
+                status = in.readUTF();
+                
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                
+            }
+        return status;
+    }
+
+    private String downloadFile(String filename) {
+        ta.setText(null);
+        Path targetPath = Paths.get("/media/sergei/Linux/ClientFiles/" + File.separator + filename);
+        Path sourcePath = Paths.get("/media/sergei/Linux/ServerFiles/" + File.separator + filename);
+        try (OutputStream outputStream = new FileOutputStream(targetPath.toFile())) {
+Files.copy(sourcePath, outputStream);
+} catch (IOException ex) {
+ex.printStackTrace();
+}
+            return "Downloaded file " + filename;
+}
+            
+    
+
+    private String showListOfFiles() {
+        
+            try {
+                
+                try {
+                    out.writeUTF("listOfFiles");
+                } catch (IOException ex) {
+                    Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                String filesList = in.readUTF();
+                ta.setText(null);
+                ta.setText(filesList);
+                
+                
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        return "Received list of files on server.";
+    }
+    
+}
---
+++ src/main/java/ru/yandex/incoming34/filestorage/Server/ClientHandler.java
@@ -0,0 +1,182 @@
+package ru.yandex.incoming34.filestorage.Server;
+
+import java.awt.List;
+import java.io.*;
+import static java.lang.String.valueOf;
+import java.net.Socket;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Обработчик входящих клиентов
+ */
+public class ClientHandler implements Runnable {
+	private final Socket socket;
+        private DataOutputStream out;
+        private DataInputStream in;
+
+	public ClientHandler(Socket socket) {
+		this.socket = socket;
+            try { 
+                out = new DataOutputStream(socket.getOutputStream());
+                in = new DataInputStream(socket.getInputStream());
+            } catch (IOException ex) {
+                Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+              
+	}
+
+	@Override
+	public void run() {
+		try{	
+                    while (true) {
+				String command = in.readUTF();
+                                switch (command) {
+                                    case "upload":
+                                    {
+                                        performUpload();
+                                        break;
+                                    }
+                                    case "remove":
+                                    {
+                                        performRemove();
+                                        break;
+                                    }
+                                    
+                                    case "download":
+                                    {
+                                        performDownload();
+                                        break;
+                                    }
+                                    case "listOfFiles":
+                                    {
+                                        showListOfFiles();
+                                        break;
+                                    }
+                                
+                                }
+                    }
+                                }   catch (IOException ex) {
+                Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+                
+                
+
+    private void performUpload() {
+        FileOutputStream fos = null;
+        try {
+		File file = new File("/media/sergei/Linux/ServerFiles" + File.separator + in.readUTF());
+		if (!file.exists()) {
+		file.createNewFile();
+                                    }
+		long size = in.readLong();
+		fos = new FileOutputStream(file);
+		byte[] buffer = new byte[256];
+                for (int i = 0; i < (size + 255) / 256; i++) { 
+                    int read = in.read(buffer);
+                    fos.write(buffer, 0, read);
+                    }
+                fos.close();
+                    
+		out.writeUTF("DONE");
+		} catch (Exception e) {
+                
+        }
+    }
+
+    private void performRemove() {
+        File file;
+            try {
+                file = new File("/media/sergei/Linux/ServerFiles" + File.separator + in.readUTF());
+            if (Objects.isNull(file) || file.getName().length() == 0) {
+            return;
+            }
+                if (file.exists()) {
+                  boolean fileDeleted = file.delete();
+                        if (fileDeleted){
+                          out.writeUTF("File " + file + " was deleted.");
+                      } else {
+                          out.writeUTF("File " + file + " was NOT deleted.");
+                      }  
+                } else {
+                    out.writeUTF("File does not exist.");
+                }
+           
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+            
+             catch (IOException ex) {
+                Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+    }
+
+    private void performDownload() {
+        File file;
+        FileInputStream fis = null;
+        try {
+        file = new File("/media/sergei/Linux/ServerFiles" + File.separator + in.readUTF());
+        if (Objects.isNull(file) || file.getName().length() == 0) {
+            out.writeLong(0);
+            return;
+        }
+			if (file.exists()) {
+                                fis = new FileInputStream(file);
+				long length = file.length();
+				out.writeLong(length);
+				int read = 0;
+				byte[] buffer = new byte[256];
+				while ((read = fis.read(buffer)) != -1) {
+					out.write(buffer, 0, read);
+				}
+				out.flush();
+				String status = in.readUTF();
+                                out.writeUTF("download - Server" + file);
+                                fis.close();
+                    
+                } else {
+                            out.writeLong(0);
+                            System.out.println("File does not exist");
+                            fis.close();
+                }
+            } catch (NullPointerException nlex) {
+                if (fis != null){
+                    try {
+                        fis.close();
+                    } catch (IOException ex) {
+                        Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                nlex.printStackTrace();
+            }
+        
+            catch (IOException ex) {
+                Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+}
+
+    private void showListOfFiles() {
+            try {
+                File dir = new File("/media/sergei/Linux/ServerFiles/");
+                File[] allFiles = dir.listFiles();
+                StringBuffer listOfFiles = new StringBuffer();
+                for (int i = 0; i < allFiles.length; i ++){
+                    String oneFile = allFiles[i].getName();
+                    listOfFiles.append(oneFile).append("\n");
+                    System.out.println(oneFile);
+                }
+                out.writeUTF(listOfFiles.toString());
+            } catch (IOException ex) {
+                Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        
+    }
+}
+
